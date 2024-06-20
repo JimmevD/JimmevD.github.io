033fbbb2490cf7ddc962846491a622ed47811e21
JimmevD <72076148+JimmevD@users.noreply.github.com>
2024-06-20 11:59:55 +0200

    [Added] Base home page

---
+++ JS/animation.js
@@ -0,0 +1,42 @@
+// Add event listener for window scroll event
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+
+    // Check if the vertical scroll position is greater than 50px
+    if (window.scrollY > 50) {
+        // Add 'scrolled' class to the header
+        header.classList.add('scrolled');
+    } else {
+        // Remove 'scrolled' class from the header
+        header.classList.remove('scrolled');
+    }
+});
+
+// Add event listener for window scroll event
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+
+    // Check if the vertical scroll position is greater than 50px
+    if (window.scrollY > 50) {
+        // Add 'scrolled' class to the nav
+        nav.classList.add('scrolled');
+    } else {
+        // Remove 'scrolled' class from the nav
+        nav.classList.remove('scrolled');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element
+    var goToTopButton = document.querySelector('.back-to-top-button');
+
+    // Add click event listener to the button
+    goToTopButton.addEventListener('click', function() {
+        // Scroll to the top of the page
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Smooth scrolling behavior
+        });
+    });
+});
+
